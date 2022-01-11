cda9870814ce93709472c0392b0ce02fb40a3c44
wayne <wayne.chiang@webcomm.com.tw>
2022-01-11 17:58:45 +0800

    week2 三版

--- src/store/index.js
+++ src/store/index.js
@@ -9,18 +9,38 @@ export default new Vuex.Store({
         uBikeList: []
     },
     mutations: {
-        setUBikeList(state, {uBikeList}) {
+        setUBikeList(state, uBikeList) {
             state.uBikeList = uBikeList
         }
     },
     actions: {
-        async getUBikeList({commit}) {
+        async fetchUBikeList({commit}) {
             try {
                 const res = await axios.get("https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json")
                 const uBikeList = res.data
                 commit('setUBikeList', uBikeList)
+                return uBikeList
             } catch (error) {
-                console.log(error)
+                console.log('fetchUBikeList error = ',error)
+            }
+        }
+    },
+    getters:{
+        // 只顯示前10筆
+        getUBikeList(state){
+            return state.uBikeList
+        },
+        doUBikePagination(state,getters) {
+            // 一頁有幾筆(1開始) ,第幾頁(1開始)
+            return (page ,index)=>{
+                if(page <1 || index < 1){return []}
+                const start = page*(index-1)
+                const end = page*(index)
+                if(getters.getUBikeList && getters.getUBikeList.length>0){
+                    return getters.getUBikeList.slice(start,end)
+                }else{
+                    return []
+                }
             }
         }
     },
--- src/views/IntroWeek2.vue
+++ src/views/IntroWeek2.vue
@@ -15,14 +15,50 @@
         :check="intro.check"
       />
       <RouterIntro v-if="intro.idx === 0" />
+      <div v-if="intro.idx === 3">
+        <div class="grid grid-cols-4 gap-4">
+          <div class="btn" @click="pageIndex--">上一頁</div>
+          <div>第{{ pageIndex }}頁</div>
+          <div class="btn" @click="pageIndex++">下一頁</div>
+        </div>
+        <!-- <div v-for="(u, idx) in uBikeList" :key="idx">ubike : {{ u.sna }}</div> -->
+        <div v-for="(u, idx) in uBikePagination" :key="idx">
+          {{ idx }} : {{ u.sna }}
+          <div class="btn" @click="setMyFavoriteStation('add', u, idx)">
+            add
+          </div>
+        </div>
+      </div>
+      <div v-if="intro.idx === 5">
+        <!-- 如果以index當key 會造成移除錯誤的ubike -->
+        <div
+          v-for="(u, idx) in myFavoriteStation"
+          :key="idx"
+          :class="{ 'bg-yellow-200': u.superLike }"
+        >
+          {{ idx }} : {{ u.sna }}
+          <div class="btn" @click="setMyFavoriteStation('pop', u, idx)">
+            pop
+          </div>
+          <div class="btn" @click="setMyFavoriteStation('splice', u, idx)">
+            splice
+          </div>
+          <div class="btn" @click="setMyFavoriteStation('super', u, idx)">
+            super
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import { fetchUBike } from "@/utils/ubike/api.js";
+// import { fetchUBike } from "@/utils/ubike/api.js";
 import AlertBell from "@/components/AlertBell.vue";
 import RouterIntro from "@/components/intro/week2/RouterIntro.vue";
+// import { mapState, mapActions, mapMutations, mapGetters } from 'vuex'
+// eslint-disable-next-line no-unused-vars
+import { mapState, mapActions, mapGetters } from "vuex";
 
 const IntroList = [
   {
@@ -51,24 +87,12 @@ const IntroList = [
   },
   {
     idx: 4,
-    title: "mixin簡介",
-    context: "ubike更新頻率為每分鐘一次 如何時間到就互叫呢？",
+    title: "promise 與 setTimeout",
+    context: "ubike更新頻率為每分鐘一次 如何時間到就呼叫呢？",
     check: false,
   },
   {
     idx: 5,
-    title: "promise 與 setTimeout",
-    context: "ubike更新頻率為每分鐘一次 如何時間到就互叫呢？",
-    check: false,
-  },
-  {
-    idx: 6,
-    title: "promise 語法糖 async await",
-    context: "吃甜甜 好過年",
-    check: false,
-  },
-  {
-    idx: 7,
     title: "陣列操作與更新",
     context: "",
     check: false,
@@ -81,18 +105,48 @@ export default {
     return {
       IntroList: IntroList,
       uBikeList: [],
+      pageIndex: 1,
+      pageSize: 10,
+      myFavoriteStation: [],
     };
   },
   async created() {
     await this.fetchUBike();
+    // await this.getUBikePagination(10, this.pageIndex);
   },
   methods: {
+    ...mapActions(["fetchUBikeList"]),
     async fetchUBike() {
-      this.uBikeList = await fetchUBike();
-      console.log(this.uBikeList);
+      await this.fetchUBikeList();
+    },
+    getUBikePagination() {
+      this.uBikeList = this.doUBikePagination(this.pageSize, this.pageIndex);
+    },
+    setMyFavoriteStation(type, ubike, idx) {
+      if (type === "add") {
+        ubike.superLike = false;
+        if (!this.myFavoriteStation.find((u) => u.sno === ubike.sno)) {
+          this.myFavoriteStation.push(ubike);
+        }else{
+          alert('不可重複加入')
+        }
+      } else if (type === "pop") {
+        this.myFavoriteStation.pop(ubike);
+      } else if (type === "splice") {
+        this.myFavoriteStation.splice(idx, 1);
+      } else if (type === "super") {
+        ubike.superLike = !ubike.superLike;
+        this.myFavoriteStation.splice(idx, 1, ubike);
+      }
     },
   },
-  computed: {},
+  computed: {
+    // ...mapState(["uBikeList"]),
+    ...mapGetters(["getUBikeList", "doUBikePagination"]),
+    uBikePagination() {
+      return this.doUBikePagination(this.pageSize, this.pageIndex);
+    },
+  },
   watch: {},
   BeforeRouteEnter(to, from, next) {
     console.log("BeforeRouteEnter");
@@ -106,5 +160,4 @@ export default {
   },
 };
 </script>
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
