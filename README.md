9a7369adbbdbe2fe718c5c3c1e376d9a54d07629
JiangWay <wayfun0312@gmail.com>
2022-01-25 01:20:25 +0800

    新增元件子傳父範例

--- README.md
+++ README.md
@@ -1,24 +1,53 @@
 # easy_to_vue
 
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Lints and fixes files
+
 ```
 npm run lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### 簡報在這
+
+#### week 1 https://docs.google.com/presentation/d/13YpOAzLuyOnaZ-E9myFd0-L63jIKyrB3JzzQSopzCFw/edit?usp=sharing
+
+#### week 2 https://docs.google.com/presentation/d/15oTAhNbdGOOSUZyIMp3WDPcZNeUg2VlTMe1VxgviJMQ/edit?usp=sharing
+
+#### week 3 https://docs.google.com/presentation/d/1b1L40QvzUQFclqZ6ULYCay-iinDIQohuC2WF53KYiQg/edit?usp=sharing
+
+### 使用技術
+
+vue-cli
+vue.js 2.x
+vuex
+vue router
+tailwindcss
+
+### 那有 Vue3 嗎？？
+
+有！範例在這 https://github.com/JiangWay/lifi_receipt_demo
+
+### bug 很多？
+
+小弟有空會慢慢修...
--- src/components/intro/week3/BindEmit.vue
+++ src/components/intro/week3/BindEmit.vue
@@ -1,14 +1,12 @@
 <template>
   <div class="flex flex-col introWeek2 border-2 border-black">
-    <div>I'm Bind One 11111</div>
+    <div>I'm Bind Emit</div>
     <div>我是String</div>
+    <!-- 直接動props會出警告 -->
     <input type="text" v-model="msg" />
 
     <div v-if="insideMsgObj.title !== 'default'">
-      <div>我是Object 沒斷開連結</div>
-      <input type="text" v-model="msgObj.title" />
-      <input type="text" v-model="msgObj.msg" />
-      <div>我是Object 斷開連結</div>
+      <div>我是Object 用emit來實現更動父值</div>
       <input type="text" v-model="insideMsgObj.title" />
       <input type="text" v-model="insideMsgObj.msg" />
     </div>
@@ -44,7 +42,33 @@ export default {
   async created() {},
   methods: {},
   computed: {},
-  watch: {},
+  watch: {
+    msg(newVal, oldVal) {
+      console.log(newVal);
+      console.log(oldVal);
+      this.$emit("update:msg", newVal);
+    },
+    // insideMsgObj(newVal, oldVal) {
+    //   console.log(newVal);
+    //   console.log(oldVal);
+    // },
+    // watch物件的方法
+    // "insideMsgObj.title"(newVal, oldVal) {
+    //   console.log(newVal);
+    //   console.log(oldVal);
+    //   this.insideMsgObj.title = newVal;
+    //   this.$emit("update:msgObj", this.insideMsgObj);
+    // },
+    insideMsgObj: {
+      handler(newVal, oldVal) {
+        console.log(newVal);
+        console.log(oldVal);
+        this.$emit("update:msgObj", newVal);
+      },
+      //當欲觀察值的特性為 call by reference，例如 Object 時，需將 deep 值設定為 true，告知 watch 需要深度觀察。否則會因為特性關係，無法觸發監聽器。
+      deep: true,
+    },
+  },
   BeforeRouteEnter(to, from, next) {
     next(() => {});
   },
